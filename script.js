5868952dec4b8d8704771ff70ed5d8eff35f4fcd
Wan Du <jhonattandc@gmail.com>
2020-12-03 11:29:10 -0400

    correciones del JS

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const tableroDeTetris = document.getElementById("tableroDeTetris");
 const espacio = tableroDeTetris.getContext("2d");
 const cuadricula = creacionDeMatriz(10, 20);
 const jugador = {
-    pos: (x = 0 , y = 0),
-    matriz: {
-        [0,0,0,],
-        [1,1,1,],
-        [0,1,0,]
-    } 
+    pos: {x:0 , y:0},
+    matriz: [
+        [0,0,0],
+        [1,1,1],
+        [0,1,0]
+    ]
 }
 espacio.scale(20, 20);
 
@@ -27,19 +27,21 @@ function creacionDeMatriz(width, height){
 
 function dibujarMatriz(matriz, offset){
     matriz.forEach((row, y) => {
-        row.forEach((value, x)) => {
-            if(value !== 0){
+        row.forEach((value, x) => {
+            if(value !==0){
                 espacio.fillStyle = "red";
-                espacio.fillRect(x + offset.x; y + offset.y, 1, 1);
+                espacio.fillRect(x + offset.x, y + offset.y, 1, 1);
             }
-        }
+        });
     });
-}
+    }
+
 
 function dibujar(){
     espacio.fillStyle = "#000";
     espacio.fillRect(0, 0, tableroDeTetris.width, tableroDeTetris.height);
-    dibujarMatriz(cuadricula, (x: 0, Y: 0));
+    dibujarMatriz(cuadricula, {x: 0, Y: 0});
+    dibujarMatriz(jugador.matriz, jugador.pos);
 }
 
 function update(time = 0){
